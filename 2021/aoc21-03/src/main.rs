a9e922b6a8124934a33b88dc6df4de793f9cc43d
Jesse Hansen <jesse.hansen@cedar.com>
2021-12-03 13:20:46 -0500

    Day 3

---
+++ 2021/aoc21-03/src/main.rs
@@ -0,0 +1,87 @@
+use std::fs;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sample = fs::read_to_string("./sample.txt")?;
+    let input = fs::read_to_string("./input.txt")?;
+
+    part1(&sample, "sample");
+    part1(&input, "input");
+
+    part2(&sample, "sample");
+    part2(&input, "input");
+
+    Ok(())
+}
+
+fn parse(contents:&str) -> Vec<String> {
+    contents.lines().into_iter().map(|x| x.to_string()).collect()
+}
+
+fn part1(contents:&str, description: &str) {
+    let contents = parse(contents);
+    let item_length = contents[0].len();
+    let half_line_count = contents.len() / 2;
+    let mut mcbs = vec![0; item_length];
+    for bit_string in contents {
+        for (pos, mcb) in mcbs.iter_mut().enumerate() {
+            if bit_string.chars().nth(pos).unwrap() == '1' {
+                *mcb = *mcb + 1;
+            }
+        }
+    }
+
+    let mut gamma:u32 = 0;
+    let mut epsilon:u32 = 0;
+    for (pos, mcb) in mcbs.iter().enumerate() {
+        let bit_pos = item_length - pos - 1;
+
+        if *mcb > half_line_count {
+            gamma |= 1 << bit_pos;
+        } else {
+            epsilon |= 1 << bit_pos;
+        }
+    }
+
+    println!("Answer Part 1 ({}) - gamma={:b}, epsilon={:b}, consumption={}", description, gamma, epsilon, gamma * epsilon);
+}
+
+fn part2(contents:&str, description: &str) {
+    let contents = parse(contents);
+    let item_length = contents[0].len();
+
+    let mut oxygen_lines = contents.clone();
+    let mut scrubber_lines = contents.clone();
+
+    let pos_vec = vec![0; item_length];
+
+    for (pos, _) in pos_vec.iter().enumerate() {
+        if oxygen_lines.len() > 1 {
+            let oxygen_mcb = get_mcb_at_pos(&oxygen_lines, pos);
+            oxygen_lines = oxygen_lines.into_iter().filter(|x| x.chars().nth(pos).unwrap() == oxygen_mcb).collect();
+        }
+        if scrubber_lines.len() > 1 {
+            let scrubber_lcb = if get_mcb_at_pos(&scrubber_lines, pos) == '1' { '0' } else { '1' };
+            scrubber_lines = scrubber_lines.into_iter().filter(|x| x.chars().nth(pos).unwrap() == scrubber_lcb).collect();
+        }
+        if oxygen_lines.len() == 1 && scrubber_lines.len() == 1 {
+            break;
+        }
+    }
+
+    let oxygen_rating = u32::from_str_radix(oxygen_lines[0].as_str(), 2).unwrap();
+    let scrubber_rating = u32::from_str_radix(scrubber_lines[0].as_str(), 2).unwrap();
+
+    println!("Answer Part 2 ({}) - oxygen={}, scrubber={}, life_support={}", description, oxygen_rating, scrubber_rating, oxygen_rating * scrubber_rating);
+}
+
+fn get_mcb_at_pos(lines:&Vec<String>, pos: usize) -> char {
+    let half_line_count = lines.len() as f64 / 2.0;
+    let line_count = lines.iter().filter(|x| x.chars().nth(pos).unwrap() == '1').count() as f64;
+
+    if line_count as f64 >= half_line_count {
+        return '1';
+    }
+
+    return '0';
+}
