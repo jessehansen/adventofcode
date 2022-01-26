3c93d98d7c04756ac5e56d47ab83f75290094d5d
Jesse Hansen <jesse.hansen@cedar.com>
2022-01-26 13:11:32 -0700

    2015 Day 14

---
+++ 2015/aoc15-14/src/main.rs
@@ -0,0 +1,158 @@
+use aoc_common::*;
+use std::collections::VecDeque;
+use std::str::FromStr;
+
+fn main() {
+    run_vec(parse_lines, part1, part2);
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Reindeer {
+    name: String,
+    speed: u32,
+    fly_time: u32,
+    rest_time: u32,
+}
+
+impl Reindeer {
+    fn race(&self, secs: u32) -> u32 {
+        let mut segments = VecDeque::from([(self.fly_time, self.speed), (self.rest_time, 0)]);
+        let mut remaining_time = secs;
+        let mut dist = 0;
+        while remaining_time > segments[0].0 {
+            let (duration, speed) = segments[0];
+            dist += duration * speed;
+            remaining_time -= duration;
+            segments.rotate_left(1);
+        }
+        dist + remaining_time * segments[0].1
+    }
+}
+
+impl FromStr for Reindeer {
+    type Err = ();
+
+    fn from_str(reindeer: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = reindeer.split_whitespace().collect();
+        Ok(Reindeer {
+            name: parts[0].to_string(),
+            speed: parts[3].parse().unwrap(),
+            fly_time: parts[6].parse().unwrap(),
+            rest_time: parts[13].parse().unwrap(),
+        })
+    }
+}
+
+enum ReindeerStatus {
+    Flying(u32),
+    Resting(u32),
+}
+
+struct RacingReindeer {
+    deer: Reindeer,
+    status: ReindeerStatus,
+    distance: u32,
+    score: u32,
+}
+
+impl RacingReindeer {
+    fn new(deer: Reindeer) -> RacingReindeer {
+        let status = ReindeerStatus::Flying(deer.fly_time);
+        RacingReindeer {
+            deer,
+            status,
+            distance: 0,
+            score: 0,
+        }
+    }
+
+    fn tick(&mut self) {
+        match self.status {
+            ReindeerStatus::Flying(duration) => {
+                self.distance += self.deer.speed;
+                self.status = if duration > 1 {
+                    ReindeerStatus::Flying(duration - 1)
+                } else {
+                    ReindeerStatus::Resting(self.deer.rest_time)
+                }
+            }
+            ReindeerStatus::Resting(duration) => {
+                self.status = if duration > 1 {
+                    ReindeerStatus::Resting(duration - 1)
+                } else {
+                    ReindeerStatus::Flying(self.deer.fly_time)
+                }
+            }
+        }
+    }
+}
+
+fn part1(reindeer: &[Reindeer]) -> u32 {
+    reindeer.iter().map(|x| x.race(2503)).max().unwrap()
+}
+
+fn race_new_score(reindeer: &[Reindeer], secs: u32) -> u32 {
+    let mut racing_deer: Vec<RacingReindeer> = reindeer
+        .iter()
+        .map(|x| RacingReindeer::new(x.clone()))
+        .collect();
+
+    for _ in 0..secs {
+        racing_deer.iter_mut().for_each(|x| x.tick());
+        let lead_distance = racing_deer.iter().map(|x| x.distance).max().unwrap();
+        racing_deer
+            .iter_mut()
+            .filter(|x| x.distance == lead_distance)
+            .for_each(|x| x.score += 1);
+    }
+
+    racing_deer.iter().map(|x| x.score).max().unwrap()
+}
+
+fn part2(reindeer: &[Reindeer]) -> u32 {
+    race_new_score(reindeer, 2503)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_race() {
+        let comet = Reindeer {
+            name: "Comet".to_string(),
+            speed: 14,
+            fly_time: 10,
+            rest_time: 127,
+        };
+        let dancer = Reindeer {
+            name: "Dancer".to_string(),
+            speed: 16,
+            fly_time: 11,
+            rest_time: 162,
+        };
+
+        assert_eq!(comet.race(1), 14);
+        assert_eq!(dancer.race(1), 16);
+        assert_eq!(comet.race(1000), 1120);
+        assert_eq!(dancer.race(1000), 1056);
+    }
+
+    #[test]
+    fn part2_test() {
+        let comet = Reindeer {
+            name: "Comet".to_string(),
+            speed: 14,
+            fly_time: 10,
+            rest_time: 127,
+        };
+        let dancer = Reindeer {
+            name: "Dancer".to_string(),
+            speed: 16,
+            fly_time: 11,
+            rest_time: 162,
+        };
+
+        assert_eq!(race_new_score(&[comet, dancer], 1000), 689);
+    }
+}
