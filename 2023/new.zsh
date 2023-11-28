1882bfb73cdb864f5c1e690125b39ce860f5b5d2
Jesse Hansen <jesse@paykeeper.com>
2023-11-28 15:45:05 -0700

    Add 2023 dir and template

---
+++ 2023/new.zsh
@@ -0,0 +1,12 @@
+number=1
+project_name="aoc23-01"
+
+while [ -d "$project_name" ]; do
+  printf -v project_name 'aoc23-%02d' "$(( ++number ))"
+done
+
+cargo new $project_name
+cd $project_name
+cp -fr ../template.rs src/main.rs
+
+echo "anyhow = \"1.0.75\"\naoc_common = { path =\"../../common\" }" >> Cargo.toml
---
+++ 2023/template.rs
@@ -0,0 +1,61 @@
+use std::str::FromStr;
+
+use anyhow::*;
+use aoc_common::*;
+
+fn main() -> Result<()> {
+    Problem::go()
+}
+
+struct Problem {}
+
+impl FromStr for Problem {
+    type Err = Error;
+
+    fn from_str(contents: &str) -> Result<Problem> {
+        Ok(Problem {})
+    }
+}
+
+impl Solution for Problem {
+    type Part1 = usize;
+    type Part2 = usize;
+
+    fn part1(&mut self) -> Result<Self::Part1> {
+        bail!("not implemented")
+    }
+
+    fn part2(&self) -> Result<Self::Part2> {
+        bail!("not implemented")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_part1() -> Result<()> {
+        let mut problem = Problem::from_str(SAMPLE)?;
+
+        let result = problem.part1()?;
+
+        assert_eq!(0, result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn sample_part2() -> Result<()> {
+        let problem = Problem::from_str(SAMPLE)?;
+
+        let result = problem.part2()?;
+
+        assert_eq!(0, result);
+
+        Ok(())
+    }
+
+    const SAMPLE: &str = "\
+";
+}
